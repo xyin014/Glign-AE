eda5b88e38f929cc52dd561a5b7ab3eed615ff87
Xizhe Yin <xyin014@ucr.edu>
2021-11-28 23:54:59 -0800

    fix input

--- ligra/ligra.h
+++ ligra/ligra.h
@@ -3881,6 +3881,7 @@ void scenario2(int argc, char* argv[]) {
   bool mmap = P.getOptionValue("-m");
   //cout << "mmap = " << mmap << endl;
   long rounds = P.getOptionLongValue("-rounds",1);
+  bool shouldShuffle = P.getOptionValue("-shuffle");
 
   string queryFileName = string(P.getOptionValue("-qf", ""));
   int combination_max = P.getOptionLongValue("-max_combination", 256);
@@ -3905,7 +3906,8 @@ void scenario2(int argc, char* argv[]) {
   std::random_device rd;
   auto rng = std::default_random_engine { rd() };
   // auto rng = std::default_random_engine {};
-  // std::shuffle(std::begin(userQueries), std::end(userQueries), rng);
+  if (shouldShuffle)
+    std::shuffle(std::begin(userQueries), std::end(userQueries), rng);
   cout << "number of random queries: " << userQueries.size() << endl;
 
   int setSize = userQueries.size();
@@ -4621,6 +4623,7 @@ void test_8(int argc, char* argv[]) {
   bool mmap = P.getOptionValue("-m");
   //cout << "mmap = " << mmap << endl;
   long rounds = P.getOptionLongValue("-rounds",1);
+  bool shouldShuffle = P.getOptionValue("-shuffle");
 
   string queryFileName = string(P.getOptionValue("-qf", ""));
   int combination_max = P.getOptionLongValue("-max_combination", 256);
@@ -4645,7 +4648,8 @@ void test_8(int argc, char* argv[]) {
   std::random_device rd;
   auto rng = std::default_random_engine { rd() };
   // auto rng = std::default_random_engine {};
-  std::shuffle(std::begin(userQueries), std::end(userQueries), rng);
+  if (shouldShuffle)
+    std::shuffle(std::begin(userQueries), std::end(userQueries), rng);
   cout << "number of random queries: " << userQueries.size() << endl;
 
   int setSize = userQueries.size();
