7d629766a5a436642948fac02235441a52987f10
Xizhe Yin <xyin014@i26.ib.hpcc.ucr.edu>
2022-09-14 16:42:03 -0700

    update

--- apps/compile.sh
+++ apps/compile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -l
 
-# git checkout ae-checkin
+git checkout ae-checkin
 make clean
 CILK=1 EDGELONG=1 LONG=1 make SSSP_Batch
 CILK=1 EDGELONG=1 LONG=1 make BFS_Batch
@@ -8,6 +8,6 @@ CILK=1 EDGELONG=1 LONG=1 make SSWP_Batch
 CILK=1 EDGELONG=1 LONG=1 make Viterbi_Batch
 CILK=1 EDGELONG=1 LONG=1 make SSNP_Batch
 
-# git checkout ae-heter
-# CILK=1 EDGELONG=1 LONG=1 make Heter_Batch
-# git checkout ae-checkin
+git checkout ae-heter
+CILK=1 EDGELONG=1 LONG=1 make Heter_Batch
+git checkout ae-checkin
